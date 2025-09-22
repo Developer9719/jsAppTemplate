74a45b6960c3424b15afb0a07735976db0c1a355
Justin <49703987+Developer9719@users.noreply.github.com>
2025-09-21 20:52:40 -0400

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -4,13 +4,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/script/index.js',
   
-  // The plugins key is a required array
   plugins: [
     new HtmlWebpackPlugin({
-        // Tell the plugin to use your new HTML file as a template
-        template: './src/index.html',
+      // Tell the plugin to use your new HTML file as a template
+      template: './src/index.html',
     }),
-  ],
+  ], 
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // This pattern matches any file that ends with .css
+        use: [
+          'style-loader', // Injects the CSS into the DOM
+          'css-loader',   // Interprets @import and url() like import/require()
+        ],
+      },
+    ],
+  },
   
   output: {
     filename: '[name].bundle.js',
