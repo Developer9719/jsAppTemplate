e69ff3d8988af835bee1673e218600ea7c99d258
Justin <49703987+Developer9719@users.noreply.github.com>
2025-09-21 20:43:53 -0400

    Update structure.js

--- src/script/structure.js
+++ src/script/structure.js
@@ -1,2 +1,19 @@
+import { display } from './interface.js';
+
 // Customize HTML template for current project
-document.title = '';
+const siteTitle = 'To-Do';
+document.title = siteTitle;
+
+// Build up main body structure
+const body = document.getElementsByTagName('body')[0];
+
+const header = document.createElement('header');
+const main = document.createElement('main');
+const footer = document.createElement('footer');
+
+body.appendChild(header);
+body.appendChild(main);
+body.appendChild(footer);
+
+// Set up page title to be viewed on the page 
+display('header', 'h1', siteTitle, undefined);
