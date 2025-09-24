ac745d9ae823669b81e5f4000305dbdf28585ea0
Justin <49703987+Developer9719@users.noreply.github.com>
2025-09-24 02:23:01 -0400

    Update index.js

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,11 @@
-import './structure.js';
-import  { createLayout } from './interface.js';
+import './pageLoad.js'; // Runs the IIFE
+import { htmlStructure } from './structure.js'; 
+import  { createLayout, interaction } from './interface.js';
+import { basicElementStructures } from './structure.js';
 
-// Called after structure is loaded 
-createLayout();
+htmlStructure(); // Setups up the basic structure
+createLayout(); // Create the customized layout
+interaction(); // Add user interaction 
+
+const createDiv = basicElementStructures.div('Testing Class. Class working for div at least'); // Dynamically creates element blueprint
+new basicElementStructures(createDiv, 'main'); // Creates the new element
